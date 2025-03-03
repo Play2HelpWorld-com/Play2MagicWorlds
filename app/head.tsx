d4d4b75f60add3685cd4d0dac7bbf6d051bf1edf
moyasiginko <mrsiyam61@gmail.com>
2025-03-03 15:09:26 +0600

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Games with Magic Worlds | Play for Free Donation</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built by MagicWorld" />
+      <meta name="description" content="Built by MagicWorlds" />
       <meta
         name="impact-site-verification"
         content="3211f99b-74dc-43e7-9fe6-193de26f04a0"
