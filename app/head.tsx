3e690f9520bcab06fe7b500fac9d996d7c3f36a0
moyasiginko <mrsiyam61@gmail.com>
2025-03-03 14:20:38 +0600

    fix metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Games with magic Worlds | Play for Free Donation</title>
+      <title>Games with Magic Worlds | Play for Free Donation</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built by MagicWorld" />
       <meta
