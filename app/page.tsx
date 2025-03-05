84ebedbf6799ccf2700e5db554f0a0d9a0708dce
moyasiginko <mrsiyam61@gmail.com>
2025-03-06 00:48:48 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
       {/* <FunFact /> */}
       {/* <CTA /> */}
       {/* <Testimonial /> */}
-      <ClaimTokenSection />
       <FAQ />
       {/* <Pricing /> */}
       {/* <Contact /> */}
